1f69fbd7de26bdb59ce0c813b1e6b79085caae50
Nicolas Williams <nico@cryptonector.com>
2012-03-11 16:43:12 -0500

    Add Dijstra algorithm for eventual capaths replacement
        The idea is to use this in the KDC for deciding how to issue
        referrals.  Clients should use default routes (really, the client
        principal's realm) to find their way to any other realm by asking
        the default route for a cross-realm krbtgt for the target realm.
        Though clients could use the shortest-path first approach, if
        they have enough information about realm trusts.
    
        (I understand that "trust" is very overloaded.  I expect that
         this will be understood nonetheless.)

--- base/test_base.c
+++ base/test_base.c
@@ -888,11 +888,129 @@ test_array()
     return 0;
 }
 
+static
+int
+test_spf(void)
+{
+    heim_dict_t g;
+    heim_dict_t paths;
+    heim_string_t str;
+    int ret;
+
+    g = heim_json_create(" { "
+			 "  \"a\" : { \"b\" : { \"distance\" : 1 },"
+			 "            \"c\" : { \"distance\" : 1 },"
+			 "            \"d\" : { \"distance\" : 1 } },"
+			 "  \"b\" : { \"b1\" : { \"distance\" : 10 },"
+			 "            \"b2\" : { \"distance\" : 10 } },"
+			 "  \"c\" : { \"c1\" : { \"distance\" : 1 },"
+			 "            \"c2\" : { \"distance\" : 1 } },"
+			 "  \"d\" : { \"d1\" : { \"distance\" : 1 },"
+			 "            \"d2\" : { \"distance\" : 1 } },"
+			 "  \"b2\" : { \"e1\" : { \"distance\" : 1 } },"
+			 "  \"c1\" : { \"e1\" : { \"distance\" : 1 } },"
+			 "  \"c2\" : { \"e1\" : { \"distance\" : 2 },"
+			 "             \"e2\" : { \"distance\" : 2 },"
+			 "             \"b2\" : { \"distance\" : 1 } },"
+			 "  \"d1\" : { \"e1\" : { \"distance\" : 1 },"
+			 "             \"e2\" : { \"distance\" : 1 },"
+			 "             \"b2\" : { \"distance\" : 5 } },"
+			 "} ", 10, 0, NULL);
+    if (!g) return ENOMEM;
+
+    ret = heim_shortest_path_first(g, HSTR("a"), NULL, &paths, NULL, NULL);
+    if (ret) return ret;
+    str = heim_serialize(paths, 0, NULL);
+    ret = strcmp(heim_string_get_utf8(str),
+"{\n"
+"	\"b\" : \n"
+"		\"a\",\n"
+"	\"c\" : \n"
+"		\"a\",\n"
+"	\"d\" : \n"
+"		\"a\",\n"
+"	\"b1\" : \n"
+"		\"b\",\n"
+"	\"b2\" : \n"
+"		\"c2\",\n"
+"	\"c1\" : \n"
+"		\"c\",\n"
+"	\"d1\" : \n"
+"		\"d\",\n"
+"	\"c2\" : \n"
+"		\"c\",\n"
+"	\"e1\" : \n"
+"		\"c1\",\n"
+"	\"d2\" : \n"
+"		\"d\",\n"
+"	\"e2\" : \n"
+"		\"d1\"\n"
+"}\n"
+);
+    if (ret)
+	return 1;
+
+    g = heim_json_create(" { "
+			 "  \"a\" : { \"b\" : { \"distance\" : 1 },"
+			 "            \"c\" : { \"distance\" : 1 },"
+			 "            \"d\" : { \"distance\" : 1 } },"
+			 "  \"b\" : { \"b1\" : { \"distance\" : 10 },"
+			 "            \"b2\" : { \"distance\" : 1 } },"
+			 "  \"c\" : { \"c1\" : { \"distance\" : 1 },"
+			 "            \"c2\" : { \"distance\" : 1 } },"
+			 "  \"d\" : { \"d1\" : { \"distance\" : 1 },"
+			 "            \"d2\" : { \"distance\" : 1 } },"
+			 "  \"b2\" : { \"e1\" : { \"distance\" : 1 } },"
+			 "  \"c1\" : { \"e1\" : { \"distance\" : 1 } },"
+			 "  \"c2\" : { \"e1\" : { \"distance\" : 2 },"
+			 "             \"e2\" : { \"distance\" : 2 },"
+			 "             \"b2\" : { \"distance\" : 1 } },"
+			 "  \"d1\" : { \"e1\" : { \"distance\" : 1 },"
+			 "             \"e2\" : { \"distance\" : 1 },"
+			 "             \"b2\" : { \"distance\" : 5 } },"
+			 "} ", 10, 0, NULL);
+    if (!g) return ENOMEM;
+
+    ret = heim_shortest_path_first(g, HSTR("a"), NULL, &paths, NULL, NULL);
+    if (ret) return ret;
+    str = heim_serialize(paths, 0, NULL);
+    ret = strcmp(heim_string_get_utf8(str),
+"{\n"
+"	\"b\" : \n"
+"		\"a\",\n"
+"	\"c\" : \n"
+"		\"a\",\n"
+"	\"d\" : \n"
+"		\"a\",\n"
+"	\"b1\" : \n"
+"		\"b\",\n"
+"	\"b2\" : \n"
+"		\"b\",\n"
+"	\"c1\" : \n"
+"		\"c\",\n"
+"	\"d1\" : \n"
+"		\"d\",\n"
+"	\"c2\" : \n"
+"		\"c\",\n"
+"	\"e1\" : \n"
+"		\"c1\",\n"
+"	\"d2\" : \n"
+"		\"d\",\n"
+"	\"e2\" : \n"
+"		\"d1\"\n"
+"}\n"
+);
+    if (ret)
+	return 1;
+    return 0;
+}
+
 int
 main(int argc, char **argv)
 {
     int res = 0;
 
+    res |= test_spf();
     res |= test_memory();
     res |= test_dict();
     res |= test_auto_release();
