cabfd44c63a36f524c117b5f7d436a03655ad651
Nico Williams <nico@cryptonector.com>
2015-03-23 19:16:10 -0500

    Add test-detach on Windows

---
+++ lib/roken/test-detach.c
@@ -0,0 +1,63 @@
+/***********************************************************************
+ * Copyright (c) 2015, Cryptonector LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **********************************************************************/
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <process.h>
+#include "roken.h"
+
+int main(int argc, char ** argv)
+{
+    char *args[3];
+
+    args[0] = argv[0];
+    args[1] = "child";
+    args[2] = NULL;
+
+    if (argc > 1) {
+	fprintf(stderr, "Child started!\n");
+    } else {
+	fprintf(stderr, "Parent started as %d\n", _getpid());
+	roken_detach_prep(argc, args);
+    }
+    fprintf(stderr, "Now should be the child: %d\n", _getpid());
+    roken_detach_finish();
+    /* This printf won't appear because stderr will be set to /dev/null */
+    fprintf(stderr, "Now should be the child: %d, wrote to parent\n", _getpid());
+    sleep(5);
+    /* Ditto */
+    fprintf(stderr, "CHILD: done\n");
+    return 0;
+}
