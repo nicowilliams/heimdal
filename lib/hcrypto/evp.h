bb4bafb95631b1138a23011c4ecfa9d7d599f949
Nicolas Williams <nico@twosigma.com>
2016-04-13 18:50:36 -0500

    fixup copyright dates

--- lib/hcrypto/evp.h
+++ lib/hcrypto/evp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 - 2008 Kungliga Tekniska Högskolan
+ * Copyright (c) 2005 - 2016 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  *
--- lib/krb5/krb5_locl.h
+++ lib/krb5/krb5_locl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-2006 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997-2016 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  *
